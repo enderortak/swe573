a185db09662026bfb534d40e0244d4b2dc37efd6
enderortak <enderortak@yahoo.com>
2019-12-13 00:30:53 +0300

    -

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -8,6 +8,7 @@ import {
   Button,
   Container,
   Divider,
+  Dropdown,
   Grid,
   Header,
   Icon,
@@ -49,19 +50,19 @@ const getWidth = () => {
 const HomepageHeading = ({ mobile }) => (
   <Container text>
     {
-    //   <Header
-    //   as="h1"
-    //   content="Get Together"
-    //   inverted
-    //   style={{
-    //     fontSize: mobile ? "2em" : "4em",
-    //     fontWeight: "normal",
-    //     marginBottom: 0,
-    //     marginTop: mobile ? "1.5em" : "3em",
-    //   }}
-    // />
+      //   <Header
+      //   as="h1"
+      //   content="Get Together"
+      //   inverted
+      //   style={{
+      //     fontSize: mobile ? "2em" : "4em",
+      //     fontWeight: "normal",
+      //     marginBottom: 0,
+      //     marginTop: mobile ? "1.5em" : "3em",
+      //   }}
+      // />
     }
-    <Image bordered rounded src={Logo} style={{display: "inline"}} />
+    <Image bordered rounded src={Logo} style={{ display: "inline" }} />
     <Header
       as="h2"
       content="Together, we are stronger!"
@@ -128,47 +129,53 @@ class DesktopContainer extends Component {
           onBottomPassedReverse={this.hideFixedMenu}
         >
           <div style={{ backgroundImage: `url(${HeroImage})` }}>
-          <Segment
-            inverted
-            textAlign="center"
-            style={{ minHeight: 700, padding: "1em 0em", background: "rgba(27, 28, 29, 0.9)" }}
-            vertical
-          >
-            <Menu
-              fixed={fixed ? "top" : null}
-              inverted={!fixed}
-              pointing={!fixed}
-              secondary={!fixed}
-              size="large"
+            <Segment
+              inverted
+              textAlign="center"
+              style={{ minHeight: 700, padding: "1em 0em", background: "rgba(27, 28, 29, 0.9)" }}
+              vertical
             >
-              <Container>
-                <Menu.Item as="a" active>
-                  Home
+              <Menu
+                fixed={fixed ? "top" : null}
+                inverted={!fixed}
+                pointing={!fixed}
+                secondary={!fixed}
+                size="large"
+              >
+                <Container>
+                  <Menu.Item as="a" active>
+                    Home
                 </Menu.Item>
-                <Menu.Item as="a">Work</Menu.Item>
-                <Menu.Item as="a">Company</Menu.Item>
-                <Menu.Item as="a">Careers</Menu.Item>
-                <Menu.Item position="right">
-                <Modal target={LogIn} trigger={
-                  <Button as="a" inverted={!fixed}>
-                    Log in {user && user.username}
-                  </Button>
-                  } />
-                <Button as="a" inverted={!fixed} style={{ marginLeft: "0.5em", paddingLeft: "2.5em" }}>
-                <Image src={ImagePlaceholder} avatar style={{ position: "absolute", left:0, }}/>
-                  Ender
-                </Button>
-                  <Modal target={SignUp} trigger={
-                  <Button as="a" inverted={!fixed} primary={fixed} style={{ marginLeft: "0.5em" }}>
-                    Sign Up
-                  </Button>
-                  } />
+                  <Menu.Item as="a">Work</Menu.Item>
+                  <Menu.Item as="a">Company</Menu.Item>
+                  <Menu.Item as="a">Careers</Menu.Item>
+                  <Menu.Item position="right">
+                    {!user &&
+                      <React.Fragment>
+                        <Modal target={LogIn} trigger={<Button as="a" inverted={!fixed}>Log in</Button>} />
+                        <Modal target={SignUp} trigger={<Button as="a" inverted={!fixed} primary={fixed} style={{ marginLeft: "0.5em" }}>Sign Up</Button>} />
+                      </React.Fragment>
+                    }
+                    {user &&
+                      <Dropdown
+                        trigger={
+                        <Button id="account-dropdown" as="a" inverted={!fixed} icon labelPosition='left' style={{ marginLeft: "0.5em", paddingRight: "2.5em" }}>
+                          <Icon><Image src={ImagePlaceholder} /></Icon>
+                          Ender
+                          <Icon name='dropdown' style={{ left: "inherit", right: 0 }} />
+                        </Button>
+                      }
+                        options={[{ key: 'signout', text: 'Sign Out', icon: 'sign-out', onClick: () => AuthService.logout() }]}
+                        direction="left"
+                        icon={null}
+                      />
 
-                </Menu.Item>
-              </Container>
-            </Menu>
-            <HomepageHeading />
-          </Segment>
+                    }
+                  </Menu.Item>
+                </Container>
+              </Menu>
+              <HomepageHeading />
+            </Segment>
           </div>
         </Visibility>
 
@@ -215,11 +222,11 @@ class MobileContainer extends Component {
           <Menu.Item as="a">Careers</Menu.Item>
           <Modal target={LogIn} trigger={
             <Menu.Item as="a">Log in</Menu.Item>
-                  } />
+          } />
 
           <Modal target={SignUp} trigger={
-                            <Menu.Item as="a">Sign Up</Menu.Item>
-                  } />
+            <Menu.Item as="a">Sign Up</Menu.Item>
+          } />
 
         </Sidebar>
 
@@ -236,9 +243,9 @@ class MobileContainer extends Component {
                   <Icon name="sidebar" />
                 </Menu.Item>
                 <Menu.Item position="right">
-                <Modal target={LogIn} trigger={
-                  <Button as="a" inverted>
-                    Log in
+                  <Modal target={LogIn} trigger={
+                    <Button as="a" inverted>
+                      Log in
                   </Button>
                   } />
 
@@ -276,182 +283,182 @@ ResponsiveContainer.propTypes = {
   children: PropTypes.node,
 }
 
-class HomepageLayout extends React.Component{
+class HomepageLayout extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        user: null
+      user: null
     };
-}
+  }
 
-componentDidMount() {
+  componentDidMount() {
     AuthService.token.subscribe(x => this.setState({ user: x ? jwt.decode(x) : null }));
-}
+  }
 
-  render(){
+  render() {
     const { user } = this.state;
     return (
-    
-  <ResponsiveContainer user={user}>
-    <Segment vertical style={{ padding: "8em 0em" }} >
-      <Container>
-    <Grid divided>
-    <Grid.Row>
-      <Grid.Column width={11} style={{ padding: "0 2rem" }}>
-        <Header as="h3" dividing content="Latest Posts" />
-        <Item.Group>
-          {
-            [ 0,1,2,3,4 ].map(i => 
-              <Modal key={`featured-${i}`} target={TestModalView} trigger={
-              <Segment style={{padding: "0.25em"}}>
 
-              <Item>
-                <Item.Image size='tiny' src={ImagePlaceholder} />
-                <Item.Content verticalAlign='middle'  style={{display: "inline-block"}}>Post {i}</Item.Content>
-              </Item>
-              </Segment>
-              } />
-              )
-          }
-        </Item.Group>
-      </Grid.Column>
-      <Grid.Column width={5}  style={{ padding: "0 2rem" }}>
-        <Header as="h3" dividing content="Featured Communities" />
-        <CommunityList />
-      </Grid.Column>
-    </Grid.Row>
-    </Grid>
-    </Container>
-    </Segment>
-    <Segment style={{ padding: "8em 0em" }} vertical>
-      <Grid container stackable verticalAlign="middle">
-        <Grid.Row>
-          <Grid.Column width={8}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              We Help Companies and Companions
-            </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              We can give your company superpowers to do things that they never thought possible.
-              Let us delight your customers and empower your needs... through pure data analytics.
-            </p>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              We Make Bananas That Can Dance
-            </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              Yes that"s right, you thought it was the stuff of dreams, but even bananas can be
-              bioengineered.
-            </p>
-          </Grid.Column>
-          <Grid.Column floated="right" width={6}>
-            <Image bordered rounded size="large" src={ImagePlaceholder} />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column textAlign="center">
-            <Button size="huge">Check Them Out</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
+      <ResponsiveContainer user={user}>
+        <Segment vertical style={{ padding: "8em 0em" }} >
+          <Container>
+            <Grid divided>
+              <Grid.Row>
+                <Grid.Column width={11} style={{ padding: "0 2rem" }}>
+                  <Header as="h3" dividing content="Latest Posts" />
+                  <Item.Group>
+                    {
+                      [0, 1, 2, 3, 4].map(i =>
+                        <Modal key={`featured-${i}`} target={TestModalView} trigger={
+                          <Segment style={{ padding: "0.25em" }}>
 
-    <Segment style={{ padding: "0em" }} vertical>
-      <Grid celled="internally" columns="equal" stackable>
-        <Grid.Row textAlign="center">
-          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              "What a Company"
+                            <Item>
+                              <Item.Image size='tiny' src={ImagePlaceholder} />
+                              <Item.Content verticalAlign='middle' style={{ display: "inline-block" }}>Post {i}</Item.Content>
+                            </Item>
+                          </Segment>
+                        } />
+                      )
+                    }
+                  </Item.Group>
+                </Grid.Column>
+                <Grid.Column width={5} style={{ padding: "0 2rem" }}>
+                  <Header as="h3" dividing content="Featured Communities" />
+                  <CommunityList />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Segment>
+        <Segment style={{ padding: "8em 0em" }} vertical>
+          <Grid container stackable verticalAlign="middle">
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <Header as="h3" style={{ fontSize: "2em" }}>
+                  We Help Companies and Companions
             </Header>
-            <p style={{ fontSize: "1.33em" }}>That is what they all say about us</p>
-          </Grid.Column>
-          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              "I shouldn"t have gone with their competitor."
-            </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              <Image avatar src="/images/avatar/large/nan.jpg" />
-              <b>Nan</b> Chief Fun Officer Acme Toys
+                <p style={{ fontSize: "1.33em" }}>
+                  We can give your company superpowers to do things that they never thought possible.
+                  Let us delight your customers and empower your needs... through pure data analytics.
             </p>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
+                <Header as="h3" style={{ fontSize: "2em" }}>
+                  We Make Bananas That Can Dance
+            </Header>
+                <p style={{ fontSize: "1.33em" }}>
+                  Yes that"s right, you thought it was the stuff of dreams, but even bananas can be
+                  bioengineered.
+            </p>
+              </Grid.Column>
+              <Grid.Column floated="right" width={6}>
+                <Image bordered rounded size="large" src={ImagePlaceholder} />
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column textAlign="center">
+                <Button size="huge">Check Them Out</Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
 
-    <Segment style={{ padding: "8em 0em" }} vertical>
-      <Container text>
-        <Header as="h3" style={{ fontSize: "2em" }}>
-          Breaking The Grid, Grabs Your Attention
+        <Segment style={{ padding: "0em" }} vertical>
+          <Grid celled="internally" columns="equal" stackable>
+            <Grid.Row textAlign="center">
+              <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
+                <Header as="h3" style={{ fontSize: "2em" }}>
+                  "What a Company"
+            </Header>
+                <p style={{ fontSize: "1.33em" }}>That is what they all say about us</p>
+              </Grid.Column>
+              <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
+                <Header as="h3" style={{ fontSize: "2em" }}>
+                  "I shouldn"t have gone with their competitor."
+            </Header>
+                <p style={{ fontSize: "1.33em" }}>
+                  <Image avatar src="/images/avatar/large/nan.jpg" />
+                  <b>Nan</b> Chief Fun Officer Acme Toys
+            </p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+
+        <Segment style={{ padding: "8em 0em" }} vertical>
+          <Container text>
+            <Header as="h3" style={{ fontSize: "2em" }}>
+              Breaking The Grid, Grabs Your Attention
         </Header>
-        <p style={{ fontSize: "1.33em" }}>
-          Instead of focusing on content creation and hard work, we have learned how to master the
-          art of doing nothing by providing massive amounts of whitespace and generic content that
-          can seem massive, monolithic and worth your attention.
+            <p style={{ fontSize: "1.33em" }}>
+              Instead of focusing on content creation and hard work, we have learned how to master the
+              art of doing nothing by providing massive amounts of whitespace and generic content that
+              can seem massive, monolithic and worth your attention.
         </p>
-        <Button as="a" size="large">
-          Read More
+            <Button as="a" size="large">
+              Read More
         </Button>
 
-        <Divider
-          as="h4"
-          className="header"
-          horizontal
-          style={{ margin: "3em 0em", textTransform: "uppercase" }}
-        >
-          <a href="#asd">Case Studies</a>
-        </Divider>
+            <Divider
+              as="h4"
+              className="header"
+              horizontal
+              style={{ margin: "3em 0em", textTransform: "uppercase" }}
+            >
+              <a href="#asd">Case Studies</a>
+            </Divider>
 
-        <Header as="h3" style={{ fontSize: "2em" }}>
-          Did We Tell You About Our Bananas?
+            <Header as="h3" style={{ fontSize: "2em" }}>
+              Did We Tell You About Our Bananas?
         </Header>
-        <p style={{ fontSize: "1.33em" }}>
-          Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
-          it"s really true. It took years of gene splicing and combinatory DNA research, but our
-          bananas can really dance.
+            <p style={{ fontSize: "1.33em" }}>
+              Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
+              it"s really true. It took years of gene splicing and combinatory DNA research, but our
+              bananas can really dance.
         </p>
-        <Button as="a" size="large">
-          I"m Still Quite Interested
+            <Button as="a" size="large">
+              I"m Still Quite Interested
         </Button>
-      </Container>
-    </Segment>
+          </Container>
+        </Segment>
 
-    <Segment inverted vertical style={{ padding: "5em 0em" }}>
-      <Container>
-        <Grid divided inverted stackable>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="About" />
-              <List link inverted>
-                <List.Item as="a">Sitemap</List.Item>
-                <List.Item as="a">Contact Us</List.Item>
-                <List.Item as="a">Religious Ceremonies</List.Item>
-                <List.Item as="a">Gazebo Plans</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="Services" />
-              <List link inverted>
-                <List.Item as="a">Banana Pre-Order</List.Item>
-                <List.Item as="a">DNA FAQ</List.Item>
-                <List.Item as="a">How To Access</List.Item>
-                <List.Item as="a">Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as="h4" inverted>
-                Footer Header
+        <Segment inverted vertical style={{ padding: "5em 0em" }}>
+          <Container>
+            <Grid divided inverted stackable>
+              <Grid.Row>
+                <Grid.Column width={3}>
+                  <Header inverted as="h4" content="About" />
+                  <List link inverted>
+                    <List.Item as="a">Sitemap</List.Item>
+                    <List.Item as="a">Contact Us</List.Item>
+                    <List.Item as="a">Religious Ceremonies</List.Item>
+                    <List.Item as="a">Gazebo Plans</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                  <Header inverted as="h4" content="Services" />
+                  <List link inverted>
+                    <List.Item as="a">Banana Pre-Order</List.Item>
+                    <List.Item as="a">DNA FAQ</List.Item>
+                    <List.Item as="a">How To Access</List.Item>
+                    <List.Item as="a">Favorite X-Men</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column width={7}>
+                  <Header as="h4" inverted>
+                    Footer Header
               </Header>
-              <p>
-                Extra space for a call to action inside the footer that could help re-engage users.
+                  <p>
+                    Extra space for a call to action inside the footer that could help re-engage users.
               </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
-    </Segment>
-    <ToastContainer />
-  </ResponsiveContainer>
-)
-        }
-    }
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Segment>
+        <ToastContainer />
+      </ResponsiveContainer>
+    )
+  }
+}
 
 export default HomepageLayout
