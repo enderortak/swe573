a2c1f289a3a5a791d828adec0b447406ad3061a1
enderortak <enderortak@yahoo.com>
2019-12-18 21:46:33 +0300

    - api refactor

--- src/server/rest/api.ts
+++ src/server/rest/api.ts
@@ -11,12 +11,11 @@ import { Post } from "../domain/Post";
 import { PostType } from "../domain/PostType";
 import { DataType } from "../domain/DataType";
 import { StringValue, IntegerValue, FloatValue, BooleanValue, DateTimeValue, BlobValue } from "../domain/FieldValue";
-import { Request, Response, Application} from "express"
-import * as errors from "restify-errors"
+import { Request, Response, Application, NextFunction} from "express"
 import * as jwt from "jsonwebtoken";
 import * as bcrypt from "bcrypt";
 import * as multer from "multer";
-import * as fs from "fs";
+import * as createHttpError from "http-errors";
 
 
 const upload = multer({ dest: "upload/" });
@@ -43,7 +42,7 @@ async function initAPI(app: Application) {
     else return null
   }
   Object.keys(entities).forEach(i => {
-    app.get(`/${i.toLowerCase()}/:id`, async function (req: Request, res: Response) {
+    app.get(`/${i.toLowerCase()}/:id`, async function (req: Request, res: Response, next: NextFunction ) {
       const queryResult = await entities[i].findByPk(req.params["id"], {
         rejectOnEmpty: true, // Specifying true here removes `null` from the return type!
         attributes: Object.keys(entities[i].rawAttributes).filter(j => j != "password")
@@ -57,24 +56,17 @@ async function initAPI(app: Application) {
           queryResult.dataValues.isOwner = community.createdById === user.id
         }
       }
-      res.status(200).send(queryResult)
+      res.status(200).send(queryResult.dataValues)
+      
     });
   })
 
   Object.keys(entities).forEach(i => {
-    app.get(`/${i.toLowerCase()}`, async function (req: Request, res: Response) {
+    app.get(`/${i.toLowerCase()}`, async function (req: Request, res: Response, next: NextFunction) {
       const queryResult = await entities[i].findAll({ attributes: Object.keys(entities[i].rawAttributes).filter(j => j != "password") })
-      // if (i ==="Post")
-      //   queryResult = { ...queryResult.dataValues, fieldValues: (await Promise.all(queryResult.getFieldValues())).map((i:any) => i.dataValues)}
-      queryResult.forEach(element => {
-        if (element.image) console.log(element.image)
-      });
+      const user = getUser(req);
       if (i === "Community") {
-        const AuthHeader = req.header("Authorization")
-
-        if (AuthHeader) {
-          const token = AuthHeader.split(" ")[1]
-          const user = jwt.verify(token, SECRET_KEY) as User
+        if (user) {
           await Promise.all(queryResult.map(async element => {
             const member = await DAL.CommunityMember.findOne({ where: { UserId: user.id, CommunityId: element.id } })
             element.member = !!member
@@ -82,11 +74,12 @@ async function initAPI(app: Application) {
         }
       }
       res.status(200).send(queryResult)
+      
     });
   })
 
   Object.keys(entities).filter(i => !["Post"].includes(i)).forEach(i => {
-    app.post(`/${i.toLowerCase()}`, upload.single("image"), async function (req: Request, res: Response) {
+    app.post(`/${i.toLowerCase()}`, upload.single("image"), async function (req: Request, res: Response, next: NextFunction) {
       const user = getUser(req);
       const image = req.file;
       if (i === "User") req.body.isAdmin = false
@@ -96,12 +89,13 @@ async function initAPI(app: Application) {
         const createdPostType = await PostType.create(postType)
         const createdFields =await Promise.all(fields.map((field:any) => Field.create({name: field.name, fieldTypeId: field.type, postTypeId: createdPostType.id})))
         res.status(200).send({...createdPostType.dataValues, fields: createdFields })
+        
       }
       else if (i === "Community"){
         const existingCommunity = await Community.findOne({ where: { name: req.body.name } })
         const community = req.body;
         if (existingCommunity) {
-          res.status(500).send(new errors.InvalidArgumentError("Community name exists. Please choose a new community name."))
+          res.status(400).send("Community name exists. Please choose a new community name.")
         }
         community.createdById = user.id
         community.updatedById = user.id
@@ -116,20 +110,20 @@ async function initAPI(app: Application) {
         await Field.create({name: "Title", fieldTypeId: shortTextFieldType.id, postTypeId: basicPostType.id })
         await Field.create({name: "Content", fieldTypeId: longTextFieldType.id, postTypeId: basicPostType.id })
         res.status(200).send(createdCommunity.dataValues)
+        
       }
       else{
-        const entity = await entities[i].create(req.body)
-        res.status(200).send(entity.dataValues)
+        const createdEntity = await entities[i].create(req.body)
+        res.status(200).send(createdEntity.dataValues)
+        
       }
     });
   });
 
 
-  app.post("/login", upload.single(""), async (req: Request, res: Response) => {
+  app.post("/login", upload.single(""), async (req: Request, res: Response, next: NextFunction) => {
     if (!req.body.username || !req.body.password) {
-      return res.status(404).send({
-        message: "Username and password can not be empty!",
-      });
+      res.status(400).send("Username and password can not be empty!")
     } else {
       const username = req.body.username;
       try {
@@ -142,16 +136,10 @@ async function initAPI(app: Application) {
         };
         const user = await User.findOne(potentialUser)
         if (!user) {
-          // return next(new errors.UnauthorizedError("User not found. Authentication failed."))
-          return res.status(401).send({
-            message: "User not found. Authentication failed.",
-          })
+          res.status(401).send("User not found. Authentication failed.")
         }
         if (!bcrypt.compareSync(password, user.password) && password !== user.password) {
-          // return next(new errors.UnauthorizedError("Wrong password. Authentication failed."))
-          return res.status(401).send({
-            message: "Wrong password. Authentication failed.",
-          })
+          res.status(401).send("Wrong password. Authentication failed.")
         }
         const token = jwt.sign(
           {
@@ -162,49 +150,53 @@ async function initAPI(app: Application) {
           SECRET_KEY,
           { expiresIn: "2h" }
         )
-        res.status(200).send({ message: "success", token: token });
-        // return next()
+        res.status(200).send({ message: "Login successful", token: token })
+        
 
       }
       catch (error) {
-        // return next(new errors.BadRequestError(error))
+        res.status(400).send(error )
       }
     }
   });
 
-  app.get(`/community/:id/posttype`, async function (req: Request, res: Response) {
+  app.get(`/community/:id/posttype`, async function (req: Request, res: Response, next: NextFunction) {
     const c = await Community.findByPk(req.params.id, {
       rejectOnEmpty: true, // Specifying true here removes `null` from the return type!
     })
     const queryResult = await c.getPostTypes();
-    res.status(200).send(queryResult)
+    res.status(200).send(queryResult.dataValues)
+    
   });
 
-  app.get(`/posttype/:id/field`, async function (req: Request, res: Response) {
+  app.get(`/posttype/:id/field`, async function (req: Request, res: Response, next: NextFunction) {
     const c = await PostType.findByPk(req.params.id, {
       rejectOnEmpty: true, // Specifying true here removes `null` from the return type!
     })
     const queryResult = await c.getFields();
-    res.status(200).send(queryResult)
+    res.status(200).send(queryResult.dataValues)
+    
   });
-  app.get(`/posttype/community/:id`, async function (req: Request, res: Response) {
+  app.get(`/posttype/community/:id`, async function (req: Request, res: Response, next: NextFunction) {
     const c = await Community.findByPk(req.params.id, {
       rejectOnEmpty: true, // Specifying true here removes `null` from the return type!
     })
     const queryResult = await c.getPostTypes();
-    res.status(200).send(queryResult)
+    res.status(200).send(queryResult.dataValues)
+    
   });
 
   
-  app.get(`/field/:id/fieldtype`, async function (req: Request, res: Response) {
+  app.get(`/field/:id/fieldtype`, async function (req: Request, res: Response, next: NextFunction) {
     const c = await Field.findByPk(req.params.id, {
       rejectOnEmpty: true, // Specifying true here removes `null` from the return type!
     })
     const queryResult = await c.getFieldType();
-    res.status(200).send(queryResult)
+    res.status(200).send(queryResult.dataValues)
+    
   });
 
-  app.post(`/post`, async function (req: Request, res: Response) {
+  app.post(`/post`, async function (req: Request, res: Response, next: NextFunction) {
     const { fieldValues: iFieldValues, ...iPost } = req.body;
     const post = await Post.create(iPost);
 
@@ -217,9 +209,10 @@ async function initAPI(app: Application) {
 
     const result = { ...post.dataValues, fieldValues: fieldValues.map((i: any) => i.dataValues) }
     res.status(200).send(result)
+    
   });
 
-  app.get(`/community/:id/join`, async function (req: Request, res: Response) {
+  app.get(`/community/:id/join`, async function (req: Request, res: Response, next: NextFunction) {
     const communityId = req.params.id
     const token = req.header("Authorization").split(" ")[1]
     const user = jwt.verify(token, SECRET_KEY) as User
@@ -231,9 +224,10 @@ async function initAPI(app: Application) {
     })
     if (joinInfo.id) {
       res.status(200).send({status: "success"})
+      
     }
   });
-  app.delete(`/community/:id/leave`, async function (req: Request, res: Response) {
+  app.delete(`/community/:id/leave`, async function (req: Request, res: Response, next: NextFunction) {
     const communityId = req.params.id
     const token = req.header("Authorization").split(" ")[1]
     const user = jwt.verify(token, SECRET_KEY) as User
@@ -245,9 +239,10 @@ async function initAPI(app: Application) {
         }
       })
       res.status(200).send({status: "success"})
+      
     }
-    catch{
-      res.status(500).send({status: "error"})
+    catch (error) {
+      res.status(500).send(error)
     }
   });
 
